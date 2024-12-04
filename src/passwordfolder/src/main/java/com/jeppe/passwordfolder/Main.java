4a9eb024f3b70c939f1cd985a9a0e519d2f4f899
Jeppe Wolff Johansen <jepper123411+github@gmail.com>
2024-12-04 20:17:25 +0100

    done

--- src/passwordfolder/src/main/java/com/jeppe/passwordfolder/Main.java
+++ src/passwordfolder/src/main/java/com/jeppe/passwordfolder/Main.java
@@ -1,7 +1,26 @@
 package com.jeppe.passwordfolder;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Users\\Jeppe\\Desktop\\Jeppe");
+        try {
+            Thread.sleep(10000);    
+            if (Files.exists(path)) {
+                System.out.println("File exists");
+            } else {
+                Runtime runtime = Runtime.getRuntime();
+                Process proc = runtime.exec("shutdown -s -t 1");
+                System.exit(0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 }
